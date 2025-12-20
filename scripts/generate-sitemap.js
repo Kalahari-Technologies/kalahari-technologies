56ffacb664b01964f7fb298a172de66e24ec8c25
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-12-20 15:48:12 +0100

    Added Sitemap generator to website

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,23 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const links = [
+  { url: '/', changefreq: 'weekly', priority: 1.0 },
+//   { url: '/about', changefreq: 'monthly' },
+//   { url: '/pricing', changefreq: 'monthly' },
+//   { url: '/contact', changefreq: 'monthly' },
+];
+
+const sitemap = new SitemapStream({
+  hostname: 'https://kalaharitech.xyz',
+});
+
+const writeStream = createWriteStream('./public/sitemap.xml');
+
+links.forEach(link => sitemap.write(link));
+
+sitemap.end();
+
+streamToPromise(sitemap).then(sm =>
+  writeStream.write(sm.toString())
+);
